c11ef406c7754c6b9299bdc0c49617f04716f9eb
srinitha062 <84458794+srinitha062@users.noreply.github.com>
2025-01-15 15:05:20 +0530

    Update providers.tf

--- terraformfiles/providers.tf
+++ terraformfiles/providers.tf
@@ -1,13 +1,3 @@
 provider "aws" {
   region = "eu-west-1"
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "team3bucket1"
-    key            = "globalstate/s3/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "team3_table"
-    encrypt        = true
-  }
-}
